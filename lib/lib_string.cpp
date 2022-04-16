c4b61fb430715ab74e650acb4754261907ead4ad
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-17 00:45:27 +0530

    made a minor changes

--- lib/lib_string.cpp
+++ lib/lib_string.cpp
@@ -2,11 +2,12 @@ namespace lib {
     bool ends_with(const std::string& major_string, const std::string& minor_string) {
         int major_string_size = major_string.size();
         int minor_string_size = minor_string.size();
-        int diff_size = major_string_size - minor_string_size;
-        if(diff_size < 0) {
+
+        if(major_string_size < minor_string_size) {
             return false;
         }
 
+        int diff_size = major_string_size - minor_string_size;
         return major_string.substr(diff_size, major_string_size - 1) == minor_string;
     }
 }
