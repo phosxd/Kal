8950be9e66ca225ca2bc43c1bbaf2739cb80ca5a
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-16 19:49:11 +0530

    kinda works now

--- arg_parser/arg_parser.cpp
+++ arg_parser/arg_parser.cpp
@@ -14,20 +14,21 @@ std::unordered_map<std::string, std::string> get_flags(char** args, const int& s
 }
 
 std::vector<std::string> get_args(char** args, const int& size) {
-    bool get_next = false;
+    int arg_start_point = 0;
+
+    std::string file_extension = ".kal";
     std::vector<std::string> prog_args;
 
-    for(int arg_itr = 0; arg_itr < size; arg_itr++) {
-        std::string file_extension = ".kal";
+    for(int arg_itr = 1; arg_itr < size; arg_itr++) {
         std::string arg_str = std::string(args[arg_itr]);
-
         if(lib::ends_with(arg_str, file_extension)) {
-            get_next = true;
+            arg_start_point = arg_itr;
+            break;
         }
+    }
 
-        if(get_next) {
-            prog_args.push_back(arg_str);
-        }
+    for(int start = arg_start_point; start < size; start++) {
+        prog_args.push_back(std::string(args[start]));
     }
 
     return prog_args;
