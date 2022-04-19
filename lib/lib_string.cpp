b777182b103715a8c4909548daf777ab316da70f
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-19 17:00:46 +0530

    added read_file() function to lib_string.cpp

--- lib/lib_string.cpp
+++ lib/lib_string.cpp
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <sstream>
+
 namespace lib {
     bool ends_with(const std::string& major_string, const std::string& minor_string) {
         int major_string_size = major_string.size();
@@ -9,5 +12,13 @@ namespace lib {
 
         int diff_size = major_string_size - minor_string_size;
         return major_string.substr(diff_size, major_string_size - 1) == minor_string;
+    }
+
+    std::string read_file(std::string file_path) {
+        std::ifstream source_file(file_path);
+        std::stringstream file_contents;
+        file_contents << source_file.rdbuf();
+        source_file.close();
+        return file_contents.str();
     }
 }
