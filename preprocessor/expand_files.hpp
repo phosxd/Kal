c9313d6fda885d32541a3e32e1020e414bf1387e
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-24 02:21:32 +0530

    still under heavy wip

--- preprocessor/expand_files.hpp
+++ preprocessor/expand_files.hpp
@@ -4,19 +4,18 @@
 #include "../lib/lib_string.hpp"
 
 namespace preproc {
-    std::string expand_files(std::string file_path) {
+    std::vector<std::string> expand_files(std::string file_path) {
         std::string file_contents = lib::read_file(file_path);
         std::vector<std::string> file_lines = lib::split(file_contents, '\n');
         int file_lines_count = file_lines.size(); 
 
         for(int current_line = 0; current_line < file_lines_count; current_line++) {
-            std::cout << file_lines[current_line] << std::endl;
             file_lines[current_line] = preproc::trim_leading(file_lines[current_line]);
             if(file_lines[current_line] == "") {
                 continue;
             }
-            std::cout << file_lines[current_line] << std::endl;;
+            file_lines[current_line] = preproc::remove_comments(file_lines[current_line]);
         }
-        return "";
+        return file_lines;
     }
 }
