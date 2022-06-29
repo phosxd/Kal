aff82f1e3d91190f56e30c173389cced3ca07f26
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-06-30 00:12:21 +0530

    added commands regarding the docker container

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+DEFAULT="\e[00;39m"
+DEFAULT_BOLD="\e[01;39m"
+
 CC="g++"
 SRC_FILE="kal.cpp"
-BIN_FILE="bin/kal-$(echo $(uname) | tr L l)-$(uname -m)"
+BIN_FILE="bin/kal-$(echo $(uname) | tr A-Z a-z)-$(uname -m)"
 OPTIMIZATION="-O2"
 FLAGS="-s -pipe -Wall -static -Werror -pedantic -fstack-protector"
 
@@ -20,6 +23,8 @@ function install() {
 
 function docker_run() {
     docker build -t kal .
+    clear
+    echo -e "${DEFAULT_BOLD}* Kal Docker Container running...${DEFAULT}"
     docker run -it --rm --name Kal kal
 }
 
