c5c3be87cd688a79688d4dd2154012cb46a633b1
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-07-12 01:23:59 +0530

    removed an unnecessary error function

--- errors.hpp
+++ errors.hpp
@@ -11,10 +11,6 @@ namespace errors {
     void kal_error(std::string kal_err) {
         std::cerr << style::red << style::bold << "Kal:" << style::reset << style::red << " " << kal_err << style::reset << std::endl;
     }
-
-    void stdout_error(std::string second_parameter) {
-        std::cerr << style::red << style::bold << "stdout:" << style::reset << style::red << " stdout takes only one parameter but multiple parameters were passed: " << second_parameter << "..." << style::reset << std::endl;
-    }
     
     void var_redeclare_error(std::string var_name, std::string var_type) {
         std::cerr << style::red << style::bold << "Variable:" << style::reset << style::red << " Variable `" << var_name << "` of type `" << var_type << "` already exists." << style::reset << std::endl;
