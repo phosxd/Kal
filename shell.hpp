aeb5ad4a0d1b8446952853aeeabdfabdaaa50452
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-07-16 00:50:58 +0530

    file inclusion now works in the shell (preprocessor)

--- shell.hpp
+++ shell.hpp
@@ -16,6 +16,16 @@ namespace shell {
             std::cout << style::bold << "\nKal" << style::reset << " " << style::bold << style::green << "[" << count << "]:" << style::reset << " ";
             std::getline(std::cin, command);
             command = preproc::remove_comments(command);
+            command = lib::trim_leading(command);
+            command = lib::trim_trailing(command);
+
+            if(command[0] == '@') {
+                std::string file_name = command.substr(1);
+                std::vector<std::string> source_lines = preproc::initial_preprocessing(file_name);
+                preproc::preprocess(source_lines);
+                std::vector<std::vector<std::string>> tokens = lexer::tokenize(source_lines);
+                line_exec(tokens, var);
+            }
 
             if(command == "") {
                 continue;
