d936792ac8242433d00558842b9835d86aa9e8a9
KILLinefficiency <shreyas.sable2166@gmail.com>
2023-07-03 20:07:43 +0530

    Added more tests for the expression parser.

--- tests/test_expr_parser.hpp
+++ tests/test_expr_parser.hpp
@@ -23,5 +23,75 @@ void test_expr_parser() {
     found = eval(expr);
     check(found, actual);
 
+    expr = "\"hello\" == \"hello\"";
+    actual = "1";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"hello\" != \"hello\"";
+    actual = "0";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"hello\" == \"hey\"";
+    actual = "0";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"hello\" != \"hey\"";
+    actual = "1";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"a\" + \"b\"";
+    actual = "\"ab\"";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"a\" * 3";
+    actual = "\"aaa\"";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"a\" + \"b\" * 3";
+    actual = "\"abbb\"";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"a\" + \"b\" * 3 == \"abbb\"";
+    actual = "1";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "(\"a\" + \"b\") * 3";
+    actual = "\"ababab\"";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "(\"a\" + \"b\") * 3 == \"ababab\"";
+    actual = "1";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "(\"a\" + \"b\") * (1 + 2) == \"ababab\"";
+    actual = "1";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"hell\" + \"o\" == \"hel\" + \"lo\"";
+    actual = "1";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"a\" * 1 + \"b\" * 2 + \"c\" * 3";
+    actual = "\"abbccc\"";
+    found = eval(expr);
+    check(found, actual);
+
+    expr = "\"a\" * 1 + \"b\" * 2 + \"c\" * 3 == \"abbccc\"";
+    actual = "1";
+    found = eval(expr);
+    check(found, actual);
+
     progress();
 }
